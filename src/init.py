1c7a9b9ce1d2b1b6a2e74d7d2b0e9c7d8008b496
spdkh <spdkh.1994@outlook.com>
2023-08-28 13:46:21 -0500

    pylint updates

---
+++ src/init.py
@@ -0,0 +1,49 @@
+"""
+    author: spdkh
+    date: Aug 2023
+"""
+import sys
+
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+from src.models import *
+
+from src.utils.config import parse_args
+
+
+# pylint: disable=W0401:
+def run():
+    """
+    Import automated modules based on arguments and build the DNN
+    for both train and prediction tasks
+    :return:
+    """
+    # parse arguments
+    args = parse_args()
+
+    if args is None:
+        sys.exit()
+
+    config = ConfigProto()
+    config.gpu_options.allow_growth = True
+    _ = InteractiveSession(config=config)
+
+    # # open session
+    if args.task is not None:
+        module_name = '.'.join(['src.models',
+                                args.task,
+                                args.dnn_type])
+    else:
+        module_name = '.'.join(['src.models',
+                                args.dnn_type])
+    print(module_name)
+    dnn_module = __import__(module_name,
+                            fromlist=[args.dnn_type.upper()])
+    dnn = getattr(dnn_module,
+                  args.dnn_type.upper())(args)
+
+    dnn.build_model()
+
+    dnn.model.load_weights(args.model_weights, by_name=True, skip_mismatch=True)
+    return dnn
